4956b8552bdbd3843c7450ae5833411cce2382eb
Om Singhal <54062830+gptaom@users.noreply.github.com>
2025-06-27 18:54:54 -0400

    Stop tracking .env file

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -9,6 +10,7 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
